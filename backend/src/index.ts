394860ff91b89ca70c0107271a43077165fceb28
Abhishek Hegde <ahegde2021@gmail.com>
2024-06-07 11:19:34 +0530

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,13 +5,17 @@ import helmet from "helmet";
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDocument from "./swagger.json";
 import { connectDb } from "./connectDb";
+import morgan from "morgan";
 
 const app = express();
+
 
 // Middleware
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 connectDb();
 
